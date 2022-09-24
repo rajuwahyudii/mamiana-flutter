5bd4a82da92480c0595c414f1ba2d39bd475db93
Rwhytm <rajuwahyudi842@gmail.com>
2022-09-24 11:01:54 +0700

    add login and register and all of packages

---
+++ lib/pages/auth/register.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text('register'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/wrapper.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mamiana/pages/auth/login.dart';
+import 'package:mamiana/pages/auth/register.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    User? firebaseUser = Provider.of<User?>(context);
+    return (firebaseUser == null) ? const Login() : const Register();
+  }
+}
